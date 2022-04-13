fa2330f76a18a6c946760331e2c15bef9fbfdb9e
Shakib202 <shakib2020360@gmail.com>
2022-04-13 16:27:22 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Header from './components/Header/Header';
-import Register from './components/Register/Register';
-import Products from './components/Products/Products';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Header from "./components/Header/Header";
+import Register from "./components/Register/Register";
+import Products from "./components/Products/Products";
+import Orders from "./components/Orders/Orders";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/products' element={<Products/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/products"
+          element={
+            <RequireAuth>
+              <Products />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </div>
   );
